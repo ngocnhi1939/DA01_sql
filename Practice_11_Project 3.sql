578efd2b0535ced393cea66a8cab458be91cbd38
ngocnhi1939 <160950727+ngocnhi1939@users.noreply.github.com>
2024-03-28 23:59:09 -0700

    Update Practice_11_Project 3.sql

--- Practice_11_Project 3.sql
+++ Practice_11_Project 3.sql
@@ -40,3 +40,26 @@ WHERE rank = 1;
 
 ---EX5---
 
+With customer_rfm As (
+Select customername,
+current_date - MAX(orderdate) As R,
+Count(Distinct ordernumber) As F,
+Sum(sales) AS M
+From public.sales_dataset_rfm_prj
+Group By customername 
+)
+,rfm_score As(
+Select customername, 
+ntile(5) Over (Order By R DESC) As r_score, 
+ntile(5) Over (order By F) As f_score, 
+ntile(5) Over (Order By M) As m_score
+From customer_rfm
+)
+,final_rfm_score As(
+Select customername, 
+Cast(r_score As varchar) || Cast(f_score As varchar)|| Cast(m_score As varchar) As rfm_score
+From rfm_score)
+Select a.customername, a.rfm_score, b.segment
+From final_rfm_score As a
+Join segment_score As b On a.rfm_score = b.scores
+Where a.rfm_score = '555'
